d4ce8b8d002b98491a04ba49b2005dfd56442fc8
Shubham <shubhamagnihotri2022@gmail.com>
2026-03-21 15:34:16 +0530

    Added authController and DTOs

--- src/main/java/com/url/shortner/security/jwt/JwtAuthenticationResponse.java
+++ src/main/java/com/url/shortner/security/jwt/JwtAuthenticationResponse.java
@@ -1,7 +1,9 @@
 package com.url.shortner.security.jwt;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class JwtAuthenticationResponse {
     private String token;
 }
--- src/main/java/com/url/shortner/security/jwt/JwtUtils.java
+++ src/main/java/com/url/shortner/security/jwt/JwtUtils.java
@@ -8,17 +8,18 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 import java.util.stream.Collectors;
-
+@Component
 public class JwtUtils {
     @Value("${jwt.secret}")
     private String jwtSecret;
-    @Value("{jwt.expiration}")
+    @Value("${jwt.expiration}")
     private int jwtExpriationMs;
     //Authorization -> Bearer <Token>
     public String getJwtFromHeader(HttpServletRequest request){
